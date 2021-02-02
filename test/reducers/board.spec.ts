61a2acf63e7fda727333b04c7c096f7049bc6cf9
Fr4nt1x <Fr4nt1x@Muc.de>
2021-02-02 15:48:27 +0100

    Add reduced test for create new board from existing

--- test/reducers/board.spec.ts
+++ test/reducers/board.spec.ts
@@ -1,14 +1,15 @@
 import boardReducer, {
   moveCardToBoard,
+  createBoard,
 } from '../../app/features/board/boardSlice';
-import createBoard from './testUtils';
+import createTestBoardWith from './testUtils';
 import { Board } from '../../app/model/board';
 
 describe('Board reducer', () => {
   describe('Moving cards from one board to another', () => {
-    const fromBoard = createBoard('Test board 1', 3, 3);
+    const fromBoard = createTestBoardWith('Test board 1', 3, 3);
     fromBoard.cardsCounter = 3;
-    const toBoard = createBoard('Test board 2', 3, 0);
+    const toBoard = createTestBoardWith('Test board 2', 3, 0);
     toBoard.cardsCounter = 10;
     const state = { byId: {} };
     state.byId[fromBoard.id] = fromBoard;
@@ -24,7 +25,6 @@ describe('Board reducer', () => {
       selectedBoard: fromBoard.id,
     };
     const result = boardReducer(state, action);
-
     const newFromBoard: Board = result.byId[fromBoard.id];
     const newToBoard: Board = result.byId[toBoard.id];
 
@@ -43,6 +43,48 @@ describe('Board reducer', () => {
 
     it('should update the cards counter on the new board when card is moved', () => {
       expect(newToBoard.cardsCounter).toBe(11);
+    });
+  });
+
+  describe('Create new board from existing', () => {
+    const numberOfColumns = 4;
+    const existingBoard = createTestBoardWith(
+      'Test board 1',
+      numberOfColumns,
+      1
+    );
+    existingBoard.columns.forEach((column, index) => {
+      existingBoard.columns[index].title = `test_title_${index}`;
+    });
+    const state = { byId: {}, allIds: [] };
+    state.byId[existingBoard.id] = existingBoard;
+
+    const payload = {
+      title: 'Newly created',
+      selectedBoardId: existingBoard.id,
+      numberOfColumns: numberOfColumns + 1, // This number should be ignored during creation, so an inconsistent state is tested
+    };
+    const action = {
+      type: createBoard,
+      payload,
+    };
+    const result = boardReducer(state, action);
+    let createdId;
+    result.allIds.forEach((element: string) => {
+      if (element !== existingBoard.id) {
+        createdId = element;
+      }
+    });
+
+    const createdBoard = result.byId[createdId];
+
+    it('should have the same number of columns as the existing board', () => {
+      expect(createdBoard.columns.length).toBe(numberOfColumns);
+    });
+    it('should have the same columns as the existing board', () => {
+      existingBoard.columns.forEach((column, index) => {
+        expect(column.title).toEqual(createdBoard.columns[index].title);
+      });
     });
   });
 });
